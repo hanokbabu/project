307dba8cb47c0cb8d8f455143086d33c7b8f89b6
hanokbabu <100519430+hanokbabu@users.noreply.github.com>
2023-10-17 05:05:33 +1100

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_data(self):
+        response = self.app.get('/')
+        self.assertIn(b"Hello, Docker!", response.data)
+        self.assertIn(b"Welcome to Software Deployment and Evolution", response.data)
+
+if __name__ == '__main__':
+    unittest.main()
