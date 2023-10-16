f4519822ad3e6aeb3b4e197e79b47b04d744732f
hanokbabu <100519430+hanokbabu@users.noreply.github.com>
2023-10-17 08:12:31 +1100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Docker! \nWelcome to Software Deployment and Evolution"
+    return "Hello, World! \nWelcome to Software Deployment and Evolution"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
